17d2a85576949d301c3969feaf05bc38a6d357c5
Thiago Mateus <thiagomateus123@hotmail.com>
2023-04-05 10:59:15 -0300

    Melhorando as mensagens de erro

--- src/common.js
+++ src/common.js
@@ -3,7 +3,11 @@ import { Alert, Platform } from 'react-native'
 const server = 'http://192.168.1.107:3000'
 
 function showError(err) {
-    Alert.alert('Ops! Ocorreu um problema!', `Messagem: ${err}`)
+    if(err.response && err.response.data){
+        Alert.alert('Ops! Ocorreu um problema!', `Messagem: ${err.response.data}`)
+    } else{
+        Alert.alert('Ops! Ocorreu um problema!', `Messagem: ${err}`)
+    }
 }
 
 function showSuccess(msg) {
